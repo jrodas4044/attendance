90f99006c631b636f70da9dd6e0391ec6ef2a7d6
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2024-08-02 21:47:10 -0600

    fixed images

--- amplify/functions/getUserPool/handler.ts
+++ amplify/functions/getUserPool/handler.ts
@@ -16,6 +16,10 @@ export const handler = async (event: any) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
+        "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
+      },  
       body: JSON.stringify(response)
     };
   } catch (error) {
